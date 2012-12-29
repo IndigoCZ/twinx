c05eed18f348bde207a775561184296592004469
Indigo <indigo@eiecon.net>
2012-12-29 21:57:50 +0100

    Remove front_page artifacts

--- test/functional/front_page_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class FrontPageControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:races)
-  end
-end
