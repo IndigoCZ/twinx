03983ae68b07275bcec663b63b6fa03b26966a0f
Indigo <indigo@eiecon.net>
2013-01-02 12:18:12 +0100

    Updated pending features spec

--- spec/features/pending_spec.rb
+++ spec/features/pending_spec.rb
@@ -2,14 +2,13 @@
 require 'spec_helper'
 
 describe "Twinx" do
-  it "provides the same management of results as for participants"
   it "can manage staff members"
   it "provides printable PDF - category starting list"
   it "provides printable PDF - category results list"
   it "provides printable PDF - county starting list"
   it "provides printable PDF - county results list"
   it "provides printable PDF - complete results"
-  it "allows multiple people to have the same resulting position"
+  it "properly handles ties"
   it "allows new county to be created when entering a participant"
   it "allows a persons birthday to be specified when entering a participant"
   it "supports import and export of race results"
