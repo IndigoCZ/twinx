6c5ebdaa066ce7b12a8cfccebef55ca2bdb876a9
Indigo <indigo@eiecon.net>
2013-05-11 18:35:10 +0200

    First implementation of Duration class

---
+++ lib/duration.rb
@@ -0,0 +1,43 @@
+class Duration
+  def initialize(ms)
+    @ms=ms
+  end
+  def self.from_ms(ms)
+    self.new(ms)
+  end
+  def self.from_string(string)
+    self.from_array(*string.split(/[:,.]/))
+  end
+  def self.from_hash(hash)
+    self.from_array(hash["min"],hash["sec"],hash["fract"])
+  end
+  def self.from_array(min,sec,msec)
+    msec=msec.to_s.ljust(3,"0").to_i
+    sec=sec.to_i
+    min=min.to_i
+    self.new((min*60000)+(sec*1000)+msec)
+  end
+  def to_i
+    return nil if @ms.nil?
+    @ms
+  end
+  def to_s
+    return nil if @ms.nil?
+    "#{min}:#{sec}.#{fract}"
+  end
+  def min
+    return "0" if @ms.nil?
+    (@ms / 60000).to_s
+  end
+
+  def sec
+    return "00" if @ms.nil?
+    ((@ms / 1000) % 60).to_s.rjust(2,"0")
+  end
+
+  def fract
+    return "000" if @ms.nil?
+    (@ms % 1000).to_s.rjust(3,"0")
+  end
+
+end
