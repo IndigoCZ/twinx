99f3d8acf5439e2f29c7a3a7f7e281865178f357
Indigo <indigo@eiecon.net>
2013-05-16 23:07:58 +0200

    New CategoryPicker

--- lib/category_picker.rb
+++ lib/category_picker.rb
@@ -1,9 +1,25 @@
 class CategoryPicker
-  def self.pick(race,options)
-    if options.has_key? :age
-      race.categories.for_gender(options[:gender]).for_age(options[:age])
-    else
-      race.categories.for_gender(options[:gender])
+  attr_reader :categories
+  def initialize(categories)
+    @categories=categories
+  end
+  def pick(options)
+    age=options[:age]
+    age||=(Time.now.year - options[:yob].to_i)
+    @categories.select do |cat|
+      category_constraint_fits_gender_or_age(cat,options[:gender],age)
+    end.sort_by(&:difficulty).reverse
+  end
+  def category_constraint_fits_gender_or_age(category,gender,age)
+    category.constraints.each do |con|
+      case con.restrict
+      when "gender"
+        return false unless con.value==gender
+      when "min_age"
+        return false unless con.value<=age
+      when "max_age"
+        return false unless con.value>=age
+      end
     end
   end
 end
