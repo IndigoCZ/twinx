90abec38e5228ed740123b0c316d4a307d2777bd
Indigo <indigo@eiecon.net>
2013-05-23 22:38:19 +0200

    First ugly full stack test of CSV import

--- spec/features/data_transfer_controller_spec.rb
+++ spec/features/data_transfer_controller_spec.rb
@@ -1,10 +1,21 @@
 require 'spec_helper'
 
 describe DataTransferController do
-  it "supports import of race results"
+  it "supports import of race results" do
+    expect {
+    @this_race=FactoryGirl.create(:race)
+    visit race_data_transfer_index_path(race_id:@this_race)
+    attach_file "import", "spec/fixtures/sample_for_import.csv"
+    click_button "Import"
+    }.to change(Person,:count).by(5)
+  end
   it "supports export of race results" do
     this_race=FactoryGirl.create(:race)
+    participant=FactoryGirl.create(:participant,race:this_race)
     visit race_data_transfer_index_path(race_id:this_race.id, format: "csv")
-    CSV.parse(page.body).should eq [["starting_no", "first_name", "last_name", "full_name", "gender", "yob", "team", "category", "position", "time", "born", "id_string"]]
+    CSV.parse(page.body).should eq [
+      ["starting_no", "first_name", "last_name", "full_name", "gender", "yob", "team", "category", "position", "time", "born", "id_string"],
+      [participant.starting_no.to_s, participant.person.first_name, participant.person.last_name, participant.person.full_name, participant.person.gender, participant.person.yob.to_s, participant.team.title, participant.category.code, "DNF",nil,participant.person.born,participant.person.id_string]
+    ]
   end
 end
