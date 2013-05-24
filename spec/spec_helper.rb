4d3c7d7b5b819768938cc03bc0474f59e3de2d91
Indigo <indigo@eiecon.net>
2013-05-24 16:35:24 +0200

    Coveralls: fixed spec_helper.rb hook

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require 'coveralls'
+Coveralls.wear!('rails')
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
@@ -9,8 +11,6 @@ Capybara.automatic_reload = false
 
 require 'database_cleaner'
 
-require 'coveralls'
-Coveralls.wear!('rails')
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
