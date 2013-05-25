203baffb760217c90411bfee66f30a8ce05a322f
Indigo <indigo@eiecon.net>
2013-05-25 12:32:57 +0200

    Added scoring test to Team

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -20,20 +20,30 @@ describe Team do
       team=FactoryGirl.create(:team,race:race)
       race.teams.should be == Team.for_race(race)
     end
-  end
-
-  context "scoring" do
-    before(:each) do
-      @team=FactoryGirl.create(:team)
-    end
-    it "provides an overall score for all participants"
-    it "provides a total of the top N scores"
     it "provides a list of DNF participants" do
+      @team=FactoryGirl.create(:team)
       a=FactoryGirl.create(:participant,team:@team)
       b=FactoryGirl.create(:participant,team:@team)
       FactoryGirl.create(:result,participant:a)
       @team.dnfs.should include b
       @team.dnfs.should_not include a
+    end
+  end
+  context "scoring" do
+    before(:each) do
+      @team=FactoryGirl.create(:team)
+      1.upto(15) do |i|
+        participant=FactoryGirl.create(:participant,team:@team)
+        Result.create(participant_id:participant.id,position:i)
+      end
+    end
+    it "provides an overall score for all participants" do
+      @team.points.should eq 71 # 12 10 9 8 7 6 5 4 3 2 1 1 1 1 1
+    end
+    it "provides a total of the top N scores" do
+      @team.points(1).should eq 12 # 12
+      @team.points(3).should eq 31 # 12 10 9
+      @team.points(9).should eq 64 # 12 10 9 8 7 6 5 4 3
     end
   end
   context "lookups" do
